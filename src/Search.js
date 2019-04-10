6878870560a70be3b076dfcb490eba26c32e16b9
Matt Nguyen <matth3wn@gmail.com>
2019-04-10 08:43:03 -0700

    linear react done

---
+++ src/Search.js
@@ -0,0 +1,53 @@
+
+import React, { Component } from 'react'
+
+let str = '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
+str = str.split(' ')
+
+export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: 0,
+      message:''
+    }
+  }
+
+  handleOnChange(num) {
+    this.setState({
+      input: num
+    })
+  }
+
+  linearSearch(arr, val) {
+    for(let i = 0; i< arr.length; i++) {
+      if(Number(arr[i]) === Number(val)){
+         this.setState({
+           message: `It took ${i+1} to find ${val}`
+         })
+         return ;
+      }
+      else{
+        this.setState({
+          message: 'Not found'
+        })
+      }
+      
+    }
+  }
+  handleOnClick() {
+    this.linearSearch(str, this.state.input)
+  }
+  render() {
+    return (
+      <div>
+
+        <label htmlFor="input">Search</label>
+        <input type="number" name='number' className="input-num" onChange={(e) => this.handleOnChange(e.target.value)} />
+        <button type="click" onClick={() => this.handleOnClick()}>Search</button>
+        {this.state.message && <p>{this.state.message}</p>}
+      </div>
+    )
+  }
+}
+
